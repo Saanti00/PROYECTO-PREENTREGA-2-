51d846954bf30b3c38fe29e72439362f04391595
Santiago Gutierrez <santiagohgutierrez1987@gmail.com>
2024-10-01 16:19:02 -0300

    productos.js

---
+++ productos.js
@@ -0,0 +1,32 @@
+const productos = [
+    {
+        id: 1,
+        img: "buzo blanco.jpeg",
+        nombre: "buzo blanco moon", 
+        precio: 14000,
+    },
+    {
+        id: 2, 
+        img: "buzo negro moon.jpeg",
+        nombre: "buzo negro moon",
+        precio: 14000,
+    },
+    {
+        id: 3, 
+        img: "buzo naranja interlude.jpeg",
+        nombre: "buzo naranja",
+        precio: 14000,
+    },
+    {
+        id: 4,
+        img: "buzo gris butterfly.jpeg",
+        nombre: "buzo butterfly gris", 
+        precio: 14000,
+    },
+    {
+        id: 5,
+        img: "buzo blanco butterfly.jpeg",
+        nombre: "buzo butterfly blanco", 
+        precio: 14000,
+    },
+  ];
